f394cc7108fd2a089dc729d92c0a6872c87c2d4c
NMMalika <94176433+NMMalika@users.noreply.github.com>
2025-12-26 12:50:24 +0300

    2. Registration  Implementing Model Forms and Editing __init__ Method

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,9 +3,32 @@ from .models import Account
 
 
 class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password',
+    }))
+
     class Meta:
         model = Account
-        fields = ['first_name', 'last_name',  'phone_number', 'email', 'password']
-        widgets = {
-            'password': forms.PasswordInput(),
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password', 'confirm_password']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Add bootstrap class to all fields
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+        # Add placeholders
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
         }
+
+        for name, field in self.fields.items():
+            if name in placeholders:
+                field.widget.attrs['placeholder'] = placeholders[name]
