6869af0578135856b3f2d556bca08ac41ef78368
NMMalika <94176433+NMMalika@users.noreply.github.com>
2025-12-27 18:42:04 +0300

    user activation link

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from .forms import RegistrationForm
 from .models import Account
@@ -15,34 +16,49 @@ def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-           first_name = form.cleaned_data['first_name']
-           last_name = form.cleaned_data['last_name']
-           email = form.cleaned_data['email']
-           phone_number = form.cleaned_data['phone_number']
-           password = form.cleaned_data['password']
-           username = email.split('@')[0]
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            username = email.split('@')[0]
 
-           user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
-           user.phone_number = phone_number
-           user.save()
-           #USER ACTIVATION VIA EMAIL TO BE IMPLEMENTED
-           current_site = get_current_site(request)
-           mail_subject = 'NEMA Ecommerce Portal Account Confirmation'
-           message = render_to_string('accounts/account_verification_email.html',{
-               'user': user,
-               'domain': current_site,
-               'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-               'token': default_token_generator.make_token(user),
-           })
-           to_email = email
-           send_email = EmailMessage(mail_subject, message, to=[to_email])
-           send_email.send()
-           
-           messages.success(request, 'Registration successful! Check your email for confirmation.', extra_tags='success_registration')
-           return render(request, 'accounts/register.html')
+            # 1. Create the user (Ensure your model default is is_active=False)
+            user = Account.objects.create_user(
+                first_name=first_name, 
+                last_name=last_name, 
+                email=email, 
+                username=username, 
+                password=password
+            )
+            user.phone_number = phone_number
+            user.save()
+
+            # 2. Email Verification Logic
+            current_site = get_current_site(request)
+            mail_subject = 'NEMA Ecommerce Portal Account Confirmation'
+            
+            # Pass the data clearly to the template
+            context = {
+                'user': user,
+                'domain': current_site.domain, # CRITICAL: use .domain
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            }
+            
+            message = render_to_string('accounts/account_verification_email.html', context)
+            
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.content_subtype = "html" # Ensures HTML rendering
+            send_email.send()
+            
+            messages.success(request, 'Registration successful! Check your email for confirmation.', extra_tags='success_registration')
+            return redirect('register') # Better to redirect than render after POST
     else:
         form = RegistrationForm()
-    context={'form': form}
+    
+    context = {'form': form}
     return render(request, 'accounts/register.html', context)
 
 def login(request):
@@ -68,3 +84,7 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You have been logged out.', extra_tags='success_logout')
     return redirect('home')
+
+def activate(request,uidb64, token):
+    # Activation logic to be implemented
+    return HttpResponse('Account activated successfully!')
